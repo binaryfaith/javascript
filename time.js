7b1bc40e7df8cf7c8a24344fc399e408ae837756
binaryfaith <binaryfaith@gmail.com>
2017-09-26 09:00:44 -0700

    finaltime

---
+++ time.js
@@ -0,0 +1,9 @@
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "AM";
+if (HOUR == 8 && MINUTE == 50 && PERIOD == "AM") {
+    console.log("It's almost 9 in the morning");
+}
+if (HOUR == 7 && MINUTE == 15 && PERIOD == "PM") {
+    console.log("It's just after 7 in the evening");
+}
