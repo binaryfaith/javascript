7720192ad2795d4161eb8f14cd69738daf2ce367
binaryfaith <binaryfaith@gmail.com>
2017-09-26 08:40:26 -0700

    idodeclarefinal

---
+++ idodeclare.js
@@ -0,0 +1,21 @@
+var favoritenumber = 1982
+var leasefavoritenumber = 13
+var pin = 8282
+var meimei = 1989
+var string1 = '1,2,3,4'
+var string2 = '5,6,7,8'
+var string3 = '9,10,11,12'
+var cssiscool = false
+var htmliscool = false
+var undefined = undefined
+
+console.log("favnum:", favoritenumber)
+console.log("least:", leasefavoritenumber)
+console.log("pin:", pin)
+console.log("meims:", meimei)
+console.log("st1:", string1)
+console.log("st2:", string2)
+console.log("st3:", "string3")
+console.log("css:", cssiscool)
+console.log("html:", htmliscool)
+console.log("undef:", undefined)
