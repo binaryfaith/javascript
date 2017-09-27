73f05aab3466feef4fe2934fe602bfbb502a6a47
binaryfaith <binaryfaith@gmail.com>
2017-09-26 18:04:12 -0700

    firstgo

---
+++ fancyarray.js
@@ -0,0 +1,7 @@
+function fancy(array) {
+    for (var i = 0; i < array.length; i++) {
+        console.log(i+ " -> "+ array[i])
+    }
+}
+var beanstalk = ["James","Jill","Jane","Jack"];
+fancy(beanstalk);
