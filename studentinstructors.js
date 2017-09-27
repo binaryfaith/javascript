cfa7b2d00484e25b9aecc432550ea946b6e9d7b7
binaryfaith <binaryfaith@gmail.com>
2017-09-26 22:03:59 -0700

    basicworks

---
+++ studentinstructors.js
@@ -0,0 +1,10 @@
+var students = [ 
+    {first_name:  'Michael', last_name : 'Jordan'},
+    {first_name : 'John', last_name : 'Rosales'},
+    {first_name : 'Mark', last_name : 'Guillen'},
+    {first_name : 'KB', last_name : 'Tonel'}
+]
+
+for (var index = 0; index < students.length; index++) {
+    console.log(students[index].first_name, students[index].last_name); 
+}
